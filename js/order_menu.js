718101164f339c8a7385fe63654848cc225bd2db
shroommm <2059035@itec.hcmus.edu.vn>
2022-03-27 10:26:57 +0700

    adding responses and creating sidebar

--- js/order_menu.js
+++ js/order_menu.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+console.log("Javascript loaded");
+
+let navBtn = document.querySelector(".nav-btn");
+let navbtnContainer = navBtn.parentElement;
+let nav = document.querySelector("#nav");
+let sideBar = document.querySelector(".sidebar")
+
+navBtn.addEventListener('click', () => {
+    console.log("nav hamburger clicked");
+    //nav.classList.toggle("nav-on");
+    sideBar.classList.toggle("nav-on");
+    // mainWrapper.classList.toggle("wrapper-off");
+    navBtn.classList.toggle("nav-btn-on");
+    navbtnContainer.classList.toggle("nav-btn-container-on");
+
+})
