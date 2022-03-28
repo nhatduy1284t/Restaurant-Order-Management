132c38dcf4d996a1fd9ca7c0b45030219087e5b1
shroommm <2059035@itec.hcmus.edu.vn>
2022-03-28 15:42:56 +0700

    update the sidebar and adding fetching api

--- js/order_menu.js
+++ js/order_menu.js
@@ -3,8 +3,21 @@ console.log("Javascript loaded");
 let navBtn = document.querySelector(".nav-btn");
 let navbtnContainer = navBtn.parentElement;
 let nav = document.querySelector("#nav");
-let sideBar = document.querySelector(".sidebar")
+let sideBar = document.querySelector(".sidebar");
+let foodGroup = document.querySelector(".food-card-group");
 
+let foodList = [];
+
+//Startup
+loadData();
+
+
+//console.log(card);
+
+
+// foodGroup.addEventListener("click", e => console.log(e.target))
+
+//Event
 navBtn.addEventListener('click', () => {
     console.log("nav hamburger clicked");
     //nav.classList.toggle("nav-on");
@@ -12,5 +25,54 @@ navBtn.addEventListener('click', () => {
     // mainWrapper.classList.toggle("wrapper-off");
     navBtn.classList.toggle("nav-btn-on");
     navbtnContainer.classList.toggle("nav-btn-container-on");
-
 })
+
+// foodGroup.addEventListener("mouseover", (e) => {
+//     console.log(e.target);
+//     // card.classList.toggle("animate__pulse");
+// })
+
+//Function
+window.myfunction = () => {
+    console.log('haha')
+}
+async function loadData() {
+    let data = await fetch("https://testapi.io/api/nhatduy1284t/resource/restaurant")
+                    .then(res => res.json())
+                    .then(json => json.data);
+    let output = "";
+    let num = 0;
+
+    data.forEach (el => {
+        let foodCard = `<div class="card d-flex align-items-center text-center" onclick=myfunction() data-id=${num}>
+                            <img src="${el.image}" class="card-img-top" alt="Food image">
+                            <div class="card-body">
+                                <h5 class="card-title">${el.name}</h5>
+                                <p class="card-text">${el.price}</p>
+                            </div>
+                        </div>`;
+        output += foodCard;
+
+        let food = {
+            id: num,
+            name: el.name,
+            price: parseInt(el.price)
+        }
+        foodList.push(food);
+
+        num++;
+    })
+
+    foodGroup.innerHTML = output;
+}
+
+
+let card = document.querySelectorAll(".card");
+console.log(card)
+card.forEach(el => {
+    el.addEventListener("click", el => {
+        console.log(el);
+    })
+    //console.log(el.classList);
+    
+});
