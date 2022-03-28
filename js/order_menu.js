68a493f22e2160402622cd72bfe1c5815cc0ef9f
shroommm <2059035@itec.hcmus.edu.vn>
2022-03-28 20:23:34 +0700

    adding food to order feature

--- js/order_menu.js
+++ js/order_menu.js
@@ -7,6 +7,7 @@ let sideBar = document.querySelector(".sidebar");
 let foodGroup = document.querySelector(".food-card-group");
 
 let foodList = [];
+let orderFoodList = [];
 
 //Startup
 loadData();
@@ -44,35 +45,110 @@ async function loadData() {
     let num = 0;
 
     data.forEach (el => {
-        let foodCard = `<div class="card d-flex align-items-center text-center" onclick=myfunction() data-id=${num}>
+        let foodCard = `<div class="card d-flex align-items-center text-center">
                             <img src="${el.image}" class="card-img-top" alt="Food image">
-                            <div class="card-body">
+                            <div class="card-body" data-id=${num}>
                                 <h5 class="card-title">${el.name}</h5>
                                 <p class="card-text">${el.price}</p>
+                                <button type="button" class="btn btn-secondary px-5 rounded-pill" onclick="addCart(${num})"><i class="fa-solid fa-cart-shopping"></i></button>
                             </div>
                         </div>`;
         output += foodCard;
 
         let food = {
-            id: num,
             name: el.name,
             price: parseInt(el.price)
         }
         foodList.push(food);
-
         num++;
     })
+
 
     foodGroup.innerHTML = output;
 }
 
-
-let card = document.querySelectorAll(".card");
-console.log(card)
-card.forEach(el => {
-    el.addEventListener("click", el => {
-        console.log(el);
-    })
-    //console.log(el.classList);
+function addCart(num) {
+    console.log(`num: ${num}\tname: ${foodList[num].name}\tprice: ${foodList[num].price}`);
+    // let table = document.querySelector(".card table tbody");
+    // table.innerHTML +=
+    console.log(checkFoodExistInCart(num), num);
+    if (checkFoodExistInCart(num)) {
+        // let foodEl = orderFoodList.find(el =>  {
+        //     if (el.food_id == num)
+        //         return true;
+        // });
+        let foodEl = orderFoodList.find(el => el.food_id == num);
+        foodEl.quantity++;  
+    }
+    else {
+        let price = foodList[num].price;
+        let name = foodList[num].name;
+        let orderFood = {
+            food_id: num,
+            quantity: 1
+        }
     
-});
+        orderFoodList.push(orderFood);
+    }
+
+
+
+    console.log(orderFoodList);
+
+    renderOrder();
+}
+
+//add event listner
+//call renderOrder()
+
+function renderOrder() {
+    let table = document.querySelector(".cart table tbody");
+    let output = "";
+    let total = 0;
+    orderFoodList.forEach(el => {
+        let n = el.food_id;
+
+        output +=  `<tr data-id=${n}>
+                        <td>${foodList[n].name}</td>
+                        <td><input class="food-quantity text-center" style="width:50px" type="text" value="${el.quantity}"  onkeypress="return isNumberKey(event)"></td>
+                        <td>${el.quantity * foodList[n].price}</td>
+                    </tr>`;
+
+        total += el.quantity * foodList[n].price;
+    })
+
+    table.innerHTML = output;
+    addEventForQuantityChange();
+    document.querySelector(".sub-total h5").textContent = total;
+}
+
+function checkFoodExistInCart(foodId) {
+    for (let i = 0; i < orderFoodList.length; i++) {
+        if (orderFoodList[i].food_id == foodId)
+            return true;
+    }
+
+    return false;
+}
+
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+
+function addEventForQuantityChange() {
+    let quantities = document.querySelectorAll(".food-quantity");
+    quantities.forEach(el => {
+        console.log(el)
+        el.addEventListener("focusout", (e) => {
+            let id = e.target.closest("tr").getAttribute("data-id");
+            console.log(e.target.value);
+
+            let foodEl = orderFoodList.find(el => el.food_id == id)
+            foodEl.quantity = e.target.value;
+            renderOrder();
+        })
+    })
+}
